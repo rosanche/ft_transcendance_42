d4f46d0817d00bb66e6a0f2b0ce483c98bf2cec1
Lubuntu <lucasmodjulien@gmail.com>
2022-06-28 15:54:02 -0400

    ajout docker-compose pour le back-end

---
+++ back-end/wait-for-postgres.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# wait-for-postgres.sh
+until PGPASSWORD=$POSTGRES_PASSWORD PGUSER=$POSTGRES_USER PGHOST=$POSTGRES_DB_HOST PGDATABASE=$POSTGRES_DB_NAME psql -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up"
+npx prisma generate
+npx prisma migrate dev
+exec "$@"
