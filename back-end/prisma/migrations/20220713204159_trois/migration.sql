63f14f527e800767dc8c9437a51cf52acb918f38
paztek <gbabeau@student.42.fr>
2022-07-13 17:46:22 -0400

    modifie migrate

--- back-end/prisma/migrations/20220713204159_trois/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[pseudo]` on the table `users` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "users_pseudo_key" ON "users"("pseudo");
--- back-end/prisma/migrations/20220713213522_/migration.sql
+++ back-end/prisma/migrations/20220713213522_/migration.sql
@@ -5,6 +5,7 @@ CREATE TABLE "users" (
     "updateAt" TIMESTAMP(3) NOT NULL,
     "email" TEXT NOT NULL,
     "hash" TEXT NOT NULL,
+    "pseudo" TEXT,
     "firstName" TEXT,
     "lastName" TEXT,
 
@@ -26,6 +27,9 @@ CREATE TABLE "bookmarks" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_pseudo_key" ON "users"("pseudo");
 
 -- AddForeignKey
 ALTER TABLE "bookmarks" ADD CONSTRAINT "bookmarks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
