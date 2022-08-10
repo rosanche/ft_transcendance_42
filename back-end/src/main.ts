1e89d4658b01b4e5308e300068e105ea51da90ac
gbabeau <gbabeau@student.42.fr>
2022-08-10 21:14:59 +0200

    envoie

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
