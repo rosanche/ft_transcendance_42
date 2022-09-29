b875436cb768e9b5732de02a2d4d6fdcd6d02924
Lucas JULIEN <ljulien@e1r11p5.clusters.42paris.fr>
2022-09-29 23:01:54 +0200

    Modification du protochat

--- front-app/src/pages/test.tsx
+++ front-app/src/pages/test.tsx
@@ -3,7 +3,9 @@ import React, { useState, useRef, useEffect } from "react";
 import { Button } from "modules/common/components/_ui/Button/Button";
 import socketio from "socket.io-client";
 import { useRouter } from "next/router";
+import Image from "next/image";
 import { Page } from "modules/common/components/_ui/Page/Page";
+import { IconMessage } from "modules/common/components/_icons/icons";
 
 type Form = {
   channel: string;
@@ -42,23 +44,47 @@ type users = {
 
 const test = () => {
   return (
-    <Page titre="test" className="W-full">
-      <div className="flex flex-1 flex-row h-5/6">
-        <RoundedContainer className="flex flex-1 mu-3 items-start mb-9">
-          <span className="text-white">ijgfuhbkfdnjkgbvjnjktbnjgfnjkbngfjlnbjkl</span>
-        </RoundedContainer>
-        <div className="flex flex-1  flex-col justify-between w-full">
-          <span className="text-white">Kelly</span>
-          <div className="flex justify-end h-8   mr-5 mb-10">
-            <input className="rounded-lg ml-5 " type="text"></input>
-            <Button
-              className="ml-[0.5rem] w-[rem4.5]"
-              variant="contained"
-              color="active"
-              onClick={() => {}}
-            >
-              envoie
-            </Button>
+    <Page titre="test" className="">
+      <div className="flex flex-1 flex-col">
+        <div className="text-center m-4 ">
+          <span className="text-white font-bold text-4xl leading-[3rem]">
+            Chat
+          </span>
+        </div>
+        <div className="flex flex-1 flex-row h-5/6">
+          <RoundedContainer className="flex w-1/3 flex-1 mu-3 items-start mb-9">
+            <span className="text-white">
+              ijgfuhbkfdnjkgbvjnjktbnjgfnjkbngfjlnbjkl
+            </span>
+          </RoundedContainer>
+          <div className="flex flex-1 flex-col ml-8 w-2/3 justify-between ">
+            <span className="text-white font-medium text-xl text-center leading-[3rem]">
+              Kelly
+            </span>
+            <RoundedContainer className="flex flex-1 mu-3 items-start mb-9">
+              <div className="w-7 h-7 shadow-sm mt-2 relative">
+                <Image
+                  layout="fill"
+                  src={"/assets/img/ljulien.jpg"}
+                  className="rounded-full"
+                />
+              </div>
+              <span className="text-white">
+                ijgfuhbkfdnjkgbvjnjktbnjgfnjkbngfjlnbjkl
+              </span>
+            </RoundedContainer>
+            <div className="flex flex-row  h-8 mb-10">
+              <input className="rounded-lg flex " type="text"></input>
+              <Button
+                className="ml-4 text-center items-center  align-middle"
+                variant="contained"
+                color="active"
+                icon={<IconMessage />}
+                onClick={() => {}}
+              >
+                envoie
+              </Button>
+            </div>
           </div>
         </div>
       </div>
