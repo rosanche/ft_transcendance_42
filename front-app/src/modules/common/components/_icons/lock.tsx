e1df8ba0525b7b5ace9ba66b0f185f6cd4ce051d
gbabeau <gbabeau@student.42.fr>
2022-10-06 22:08:21 +0200

    rajout lock

--- front-app/src/modules/common/components/_icons/lock.tsx
+++ front-app/src/modules/common/components/_icons/lock.tsx
@@ -10,14 +10,14 @@ const IconLock = (props) => (
   >
     <path
       d="M8 10V7V7C8 4.791 9.791 3 12 3V3C14.209 3 16 4.791 16 7V7V10"
-      stroke="#414C5E"
+      stroke="currentColor"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
       d="M12 14V17"
-      stroke="#414C5E"
+      stroke="currentColor"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
@@ -26,7 +26,7 @@ const IconLock = (props) => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M17 21H7C5.895 21 5 20.105 5 19V12C5 10.895 5.895 10 7 10H17C18.105 10 19 10.895 19 12V19C19 20.105 18.105 21 17 21Z"
-      stroke="#414C5E"
+      stroke="currentColor"
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
